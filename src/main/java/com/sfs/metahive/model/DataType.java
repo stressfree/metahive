2f285ceb3ac200b3d2d635f3af11b76af59e150e
David Harrison <david.harrison@stress-free.co.nz>
2012-01-16 14:58:57 +1300

    Tweaked the DataType class to include :calculators for the different data types.

--- src/main/java/com/sfs/metahive/model/DataType.java
+++ src/main/java/com/sfs/metahive/model/DataType.java
@@ -28,33 +28,32 @@ public enum DataType {
     	
     	Collection<KeyValueGenerator> keyValueGens = new ArrayList<KeyValueGenerator>();
     	
-    	if (this == DataType.TYPE_UNIQUEID) {
-    		keyValueGens.add(KeyValueGenerator.NOT_APPLICABLE);
-    	}    	
-    	if (this == DataType.TYPE_STRING) {
-    		keyValueGens.add(KeyValueGenerator.NEWEST);
-    		keyValueGens.add(KeyValueGenerator.OLDEST);
-    		keyValueGens.add(KeyValueGenerator.FREQUENT_DEFAULT_NEW);
-    		keyValueGens.add(KeyValueGenerator.FREQUENT_DEFAULT_NEW);
-    	}    	
+		keyValueGens.add(KeyValueGenerator.NEWEST);
+		keyValueGens.add(KeyValueGenerator.OLDEST);
+		keyValueGens.add(KeyValueGenerator.FREQUENT_DEFAULT_NEW);
+		keyValueGens.add(KeyValueGenerator.FREQUENT_DEFAULT_OLD);
+    	
     	if (this == DataType.TYPE_NUMBER || this == DataType.TYPE_PERCENTAGE) {
-    		keyValueGens.add(KeyValueGenerator.NEWEST);
-    		keyValueGens.add(KeyValueGenerator.OLDEST);
     		keyValueGens.add(KeyValueGenerator.AVERAGE);
     		keyValueGens.add(KeyValueGenerator.HIGHEST);
     		keyValueGens.add(KeyValueGenerator.LOWEST);
-    		keyValueGens.add(KeyValueGenerator.MEDIAN);
-    		keyValueGens.add(KeyValueGenerator.QUARTILE_LOWER);
-    		keyValueGens.add(KeyValueGenerator.QUARTILE_UPPER);
     	}
+    	
     	if (this == DataType.TYPE_BOOLEAN) {
-    		keyValueGens.add(KeyValueGenerator.NEWEST);
-    		keyValueGens.add(KeyValueGenerator.OLDEST);
     		keyValueGens.add(KeyValueGenerator.UNCLEAR);
+    	}
+    	
+    	if (this == DataType.TYPE_NUMBER || this == DataType.TYPE_PERCENTAGE
+    			|| this == DataType.TYPE_BOOLEAN) {
     		keyValueGens.add(KeyValueGenerator.MEDIAN);
     		keyValueGens.add(KeyValueGenerator.QUARTILE_LOWER);
     		keyValueGens.add(KeyValueGenerator.QUARTILE_UPPER);
     	}
+    	
+    	if (this == DataType.TYPE_UNIQUEID) {
+    		keyValueGens = new ArrayList<KeyValueGenerator>();
+    		keyValueGens.add(KeyValueGenerator.NOT_APPLICABLE);
+    	}  
     	return keyValueGens;
     }
 }
