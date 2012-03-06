1be6efb883eb0d82af24afd3fc334dcf03770327
David Harrison <david.harrison@stress-free.co.nz>
2012-03-06 16:46:09 +1300

    Initial implementation of the advanced search screen - needs more work.

--- src/main/java/com/sfs/metahive/web/RecordController.java
+++ src/main/java/com/sfs/metahive/web/RecordController.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     David Harrison, Triptech Ltd - initial API and implementation
  ******************************************************************************/
@@ -113,6 +113,12 @@ public class RecordController extends BaseController {
     public String bulkForm(Model uiModel) {
         uiModel.addAttribute("record", new RecordForm());
         return "records/bulk";
+    }
+
+    @RequestMapping(value = "/advanced", method = RequestMethod.GET)
+    public String advancedSearch(Model uiModel) {
+        uiModel.addAttribute("search", new RecordForm());
+        return "records/advanced";
     }
 
     /**
